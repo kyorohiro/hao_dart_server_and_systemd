fc78a909d1efb7c4f7d995d4a3d9da3fbf613184
kyorohiro <kyorohiro@gmail.com>
2021-04-04 08:20:16 +0900

    certbot well-know path readme

--- README.md
+++ README.md
@@ -65,3 +65,37 @@ $ systemctl enable systemd-networkd
 $ systemctl enable systemd-networkd-wait-online
 ```
 
+
+## Let's encrypt
+
+- get ssl  
+
+```
+$ apt-get install certbot -y
+$ certbot certonly --webroot -w /var/www/html -d tetorica.net -m kyorohiro@gmail.com
+Saving debug log to /var/log/letsencrypt/letsencrypt.log
+Plugins selected: Authenticator webroot, Installer None
+Obtaining a new certificate
+Performing the following challenges:
+http-01 challenge for tetorica.net
+Using the webroot path /var/www/html for all unmatched domains.
+Waiting for verification...
+Cleaning up challenges
+
+IMPORTANT NOTES:
+ - Congratulations! Your certificate and chain have been saved at:
+   /etc/letsencrypt/live/tetorica.net/fullchain.pem
+   Your key file has been saved at:
+   /etc/letsencrypt/live/tetorica.net/privkey.pem
+   Your cert will expire on 2021-07-02. To obtain a new or tweaked
+   version of this certificate in the future, simply run certbot
+   again. To non-interactively renew *all* of your certificates, run
+   "certbot renew"
+ - If you like Certbot, please consider supporting our work by:
+
+   Donating to ISRG / Let's Encrypt:   https://letsencrypt.org/donate
+   Donating to EFF:                    https://eff.org/donate-le
+```
+
+/etc/letsencrypt/, /var/log/letsencrypt/, /var/lib/letsencrypt/
+
--- bin/main.dart
+++ bin/main.dart
@@ -8,19 +8,24 @@ void main(List<String> arguments) async {
     var httpServer = await io.HttpServer.bind("0.0.0.0", 80);
     print("binded");
     await for (var request in httpServer) {
-      print("receive requested ${request.uri}");
-      if (request.uri.path.startsWith("/.well-known/")) {
-        var acmeChallengeFilePath = "" +
-            cerbotWebRootPath +
-            request.uri.path.replaceAll(RegExp("\\?.*"), "");
-        acmeChallengeFilePath = acmeChallengeFilePath.replaceAll("/..", "/");
-        var acmeChallengeFile = io.File(acmeChallengeFilePath);
-        var acmeChallengeData = await acmeChallengeFile.readAsString();
-        request.response.write(acmeChallengeData);
+      try {
+        print("receive requested ${request.uri}");
+        if (request.uri.path.startsWith("/.well-known/")) {
+          var acmeChallengeFilePath = "" +
+              cerbotWebRootPath +
+              request.uri.path.replaceAll(RegExp("\\?.*"), "");
+          acmeChallengeFilePath = acmeChallengeFilePath.replaceAll("/..", "/");
+          var acmeChallengeFile = io.File(acmeChallengeFilePath);
+          var acmeChallengeData = await acmeChallengeFile.readAsString();
+          request.response.write(acmeChallengeData);
+          request.response.close();
+        }
+        request.response.write("Hello");
         request.response.close();
+      } catch (e, s) {
+        print("${e}");
+        print("${s}");
       }
-      request.response.write("Hello");
-      request.response.close();
     }
   } catch (e, s) {
     print("${e}");
