46a07a254852368aadaa05743c2340483be38ddb
kyorohiro <kyorohiro@gmail.com>
2021-04-04 08:39:21 +0900

    add ssl function

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,12 @@
 import 'dart:io' as io;
 
 const String cerbotWebRootPath = "/var/www/html";
-
+const String privkeyPath = "/etc/letsencrypt/live/tetorica.net/privkey.pem";
+const String fullchainPath = "/etc/letsencrypt/live/tetorica.net/fullchain.pem";
 void main(List<String> arguments) async {
   try {
     print("start bind");
-    var httpServer = await io.HttpServer.bind("0.0.0.0", 80);
-    print("binded");
-    await for (var request in httpServer) {
+    onRequest(io.HttpRequest request) async {
       try {
         print("receive requested ${request.uri}");
         if (request.uri.path.startsWith("/.well-known/")) {
@@ -27,6 +26,22 @@ void main(List<String> arguments) async {
         print("${s}");
       }
     }
+
+    var httpServer = await io.HttpServer.bind("0.0.0.0", 80);
+    print("binded 80");
+    httpServer.listen((request) {
+      onRequest(request);
+    });
+
+    String key = io.Platform.script.resolve(privkeyPath).toFilePath();
+    String crt = io.Platform.script.resolve(fullchainPath).toFilePath();
+    io.SecurityContext context = new io.SecurityContext();
+    context.useCertificateChain(crt);
+    context.usePrivateKey(key, password: "");
+    var httpsServer = await io.HttpServer.bindSecure("0.0.0.0", 443, context);
+    httpsServer.listen((request) {
+      onRequest(request);
+    });
   } catch (e, s) {
     print("${e}");
     print("${s}");
